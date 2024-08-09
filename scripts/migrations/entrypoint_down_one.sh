c4e9e3776762503e1e9401f01eec9a640d0c9908
new laptop <vladimir.frolov.99@mail.ru>
2024-08-09 18:15:10 +0300

    copy migration scripts

---
+++ scripts/migrations/entrypoint_down_one.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Применим миграции
+# ВАЖНО! Для локального запуска вызвать из корня проекта, предварительно подгрузив переменные окружения в терминал командой  `set -a; source <путь к .env файлу>; set +a`
+# Установка goose: go install github.com/pressly/goose/v3/cmd/goose@latest
+goose -dir ./migrations postgres "user=$notification_SERVICE_POSTGRES_USER password=$notification_SERVICE_POSTGRES_PASSWORD dbname=$notification_SERVICE_POSTGRES_DB host=$notification_SERVICE_POSTGRES_HOST port=$notification_SERVICE_POSTGRES_PORT sslmode=disable" down
