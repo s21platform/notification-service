bb3dc1336c0a893a00b83db8e9c41596adc255f4
new laptop <vladimir.frolov.99@mail.ru>
2024-08-09 18:21:22 +0300

    fix scripts

--- scripts/migrations/entrypoint_up_all.sh
+++ scripts/migrations/entrypoint_up_all.sh
@@ -3,4 +3,4 @@
 # Применим миграции
 # ВАЖНО! Для локального запуска вызвать из корня проекта, предварительно подгрузив переменные окружения в терминал командой  `set -a; source <путь к .env файлу>; set +a`
 # Установка goose: go install github.com/pressly/goose/v3/cmd/goose@latest
-goose -dir ./migrations postgres "user=$notification_SERVICE_POSTGRES_USER password=$notification_SERVICE_POSTGRES_PASSWORD dbname=$notification_SERVICE_POSTGRES_DB host=$notification_SERVICE_POSTGRES_HOST port=$notification_SERVICE_POSTGRES_PORT sslmode=disable" up
+goose -dir ./migrations postgres "user=$NOTIFICATION_SERVICE_POSTGRES_USER password=$NOTIFICATION_SERVICE_POSTGRES_PASSWORD dbname=$NOTIFICATION_SERVICE_POSTGRES_DB host=$NOTIFICATION_SERVICE_POSTGRES_HOST port=$NOTIFICATION_SERVICE_POSTGRES_PORT sslmode=disable" up
